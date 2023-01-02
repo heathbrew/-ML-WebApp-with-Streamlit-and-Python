7c7bf91140eeedaa43028d73a0dc99fb807db7da
Ayushman Pranav <55629425+heathbrew@users.noreply.github.com>
2023-01-02 18:11:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-!pip install sklearn
-!pip install scikitplot
+!python -m pip install scikit-learn
+!python -m pip install scikit-learn
 import streamlit as st
 import pandas as pd
 import numpy as np
