635b1ba9ef06ce6cd2789f4e45f668b008e38d51
Ayushman Pranav <55629425+heathbrew@users.noreply.github.com>
2023-01-02 18:11:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-!python -m pip install scikit-learn
-!python -m pip install scikit-learn
+python -m pip install scikit-learn
+python -m pip install scikit-learn
 import streamlit as st
 import pandas as pd
 import numpy as np
